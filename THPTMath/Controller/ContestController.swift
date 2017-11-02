1eda9c07b8bd5047902a56ef48380e4ac6ecc086
hugleMr <lehungcva@gmail.com>
2017-11-02 21:38:42 +0700

    add callBackNative

--- THPTMath/Controller/ContestController.swift
+++ THPTMath/Controller/ContestController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 import SwiftyJSON
+import WebKit
 
-class ContestController: ViewController {
+class ContestController: ViewController,WKUIDelegate,WKScriptMessageHandler {
 
+    var webView: WKWebView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupNavigationBar();
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        let contentController = WKUserContentController();
+        contentController.add(self,name: "callbackHandler")
+        
+        let config = WKWebViewConfiguration()
+        config.userContentController = contentController
+        
+        webView = WKWebView(frame: CGRect.zero, configuration: config)
+        webView?.uiDelegate = self
+        view = webView
+        _ = webView?.load(URLRequest(url: URL(fileURLWithPath: Bundle.main.path(forResource: "test", ofType: "html")!)))
+    }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage)
+    {
+        if(message.name == "callbackHandler") {
+            print("JavaScript is sending a message \(message.body)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
